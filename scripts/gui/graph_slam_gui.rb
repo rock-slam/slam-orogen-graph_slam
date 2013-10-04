71aa930a105f23767a4bd5c594bc6ebbae438c9a
Sascha Arnold <sascha.arnold@dfki.de>
2013-10-04 16:23:42 +0200

    added the possibility to handle an async task context in the graph slam gui

--- scripts/gui/graph_slam_gui.rb
+++ scripts/gui/graph_slam_gui.rb
@@ -1,20 +1,9 @@
 def load_graph_slam_gui(velodyne_slam_task)
     widget = Vizkit::load File.join(File.dirname(__FILE__),'graph_slam_gui.ui')
-    Vizkit.vizkit3d_widget = widget.vizkit_widget
-
-    debug_reader = velodyne_slam_task.debug_information.reader
-
     semaphore = Mutex.new
-    graphviz_timer = Qt::Timer.new
     graphics_scene = Qt::GraphicsScene.new
     widget.graph_viz_img_view.scene = graphics_scene
-    graphviz_timer.connect(SIGNAL('timeout()')) do
-        sample = debug_reader.read_new
-        if not sample.nil? then
-            semaphore.synchronize { widget.graph_viz_dot.plainText = sample.graphviz }
-        end
-    end
-    graphviz_timer.start(5000)
+    Vizkit.vizkit3d_widget = widget.vizkit_widget
 
     widget.update_graph_viz.connect(SIGNAL('released()')) do
         dot_file = File.new("/tmp/graph_viz.dot", "w")
@@ -27,8 +16,30 @@ def load_graph_slam_gui(velodyne_slam_task)
         end
     end
 
-    Vizkit.display velodyne_slam_task.envire_map
-    Vizkit.display velodyne_slam_task.pose_samples, :widget => Vizkit.default_loader.RigidBodyStateVisualization
+    if velodyne_slam_task.is_a?(Orocos::Async::TaskContextProxy) then
+        velodyne_slam_task.port("debug_information").on_data do |data|
+            semaphore.synchronize { widget.graph_viz_dot.plainText = data.graphviz }
+        end
+
+        velodyne_slam_task.port("envire_map").once_on_reachable do
+            Vizkit.display velodyne_slam_task.port("envire_map")
+        end
+
+        velodyne_slam_task.port("pose_samples").once_on_reachable do
+            Vizkit.display velodyne_slam_task.port("pose_samples"), :widget => Vizkit.default_loader.RigidBodyStateVisualization
+        end
+
+    elsif velodyne_slam_task.is_a?(Orocos::TaskContext)
+        velodyne_slam_task.debug_information.connect_to do |data,_|
+            semaphore.synchronize { widget.graph_viz_dot.plainText = data.graphviz }
+        end
+
+        Vizkit.display velodyne_slam_task.envire_map
+        Vizkit.display velodyne_slam_task.pose_samples, :widget => Vizkit.default_loader.RigidBodyStateVisualization
+    else
+        puts "error velodyne_slam_task is no valid TaskContext"
+        exit
+    end
 
     widget.show
     widget
