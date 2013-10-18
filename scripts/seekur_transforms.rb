4e324306965288106cab915d88261ed44f03aeb7
Sascha Arnold <sascha.arnold@dfki.de>
2013-10-18 13:38:42 +0200

    added new transformations to test scripts

--- scripts/seekur_transforms.rb
+++ scripts/seekur_transforms.rb
@@ -1,4 +1,10 @@
 static_transform(Eigen::Vector3.new(-0.503,0.0,1.364),
                  Eigen::Quaternion.from_euler(Eigen::Vector3.new(-0.5*Math::PI,0,0),2,1,0),"laser" => "body")
 
+static_transform(Eigen::Vector3.new(0.0,0.0,0.0),
+		 Eigen::Quaternion.from_euler(Eigen::Vector3.new(0,0,0),2,1,0),"map" => "world")
+
+static_transform(Eigen::Vector3.new(0.0,0.0,0.0),
+		 Eigen::Quaternion.from_euler(Eigen::Vector3.new(0,0,0),2,1,0),"odometry" => "world")
+
 dynamic_transform "odometry.odometry_samples", "body" => "odometry"
